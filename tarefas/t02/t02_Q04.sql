697b29daa557747bdae66be4c2a07525eed8f09d
rickEDU <77811164+rickEDU@users.noreply.github.com>
2022-05-23 19:23:25 -0300

    Questão 05 da tarefa 02 #2

--- tarefas/t02/t02_Q04.sql
+++ tarefas/t02/t02_Q04.sql
@@ -2,11 +2,11 @@ CREATE FUNCTION inserir_milhas()
 RETURNS trigger AS $cartao_milhagem$
 BEGIN
   INSERT INTO milhas(cliente, quantidade) 
-  values (NEW.codigo, 0);
+  VALUES (NEW.codigo, 0);
 END;
 $cartao_milhagem$ LANGUAGE plpgsql;
 
-create trigger cartao_milhagem
-    after insert on cliente
-    for each row
-    execute function inserir_milhas();
+CREATE TRIGGER cartao_milhagem
+    AFTER INSERT ON cliente
+    FOR EACH ROW
+    EXECUTE FUNCTION inserir_milhas();
---
+++ tarefas/t02/t02_Q05.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION adiciona_voo() 
+RETURNS TRIGGER AS $soma_voo$
+    BEGIN
+        UPDATE piloto
+		SET num_voos = num_voos + 1
+		WHERE codigo = new.piloto;
+		
+		RETURN NEW;
+    END;
+	
+$add_voo$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER soma_voo
+    AFTER INSERT ON voo
+    FOR EACH ROW
+    EXECUTE FUNCTION adiciona_voo();
